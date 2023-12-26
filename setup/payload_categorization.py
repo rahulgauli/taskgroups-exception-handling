367827a3b02ba1a4c7bfe18ee6bfcd865578b114
rahulgauli <rahul.gauli1997@gmail.com>
2023-12-25 22:46:33 -0600

    remove nuissance

--- setup/payload_categorization.py
+++ setup/payload_categorization.py
@@ -1,19 +1,4 @@
-# payload = {
-#     "primary_requirements": {
-#         "hashicorp_vault_namespace_name":"namespace_name",
-#         "ad_group_admins": "admin_group_name",
-#         "ad_group_reader": "reader_group_name"},
-#     "add_on_services": {
-#         "auth_methods": {
-#             "kubernetes_auth_engine": {"cluster_names":["cluster_name_1", "cluster_name_2"], "kubernetes_namespace": "namespace_name"},
-#             "ad_auth_engine": True,
-#             "github_oidc_auth": False,
-#         },
-#         "secrets_engines": {
-#             "ldap_secrets_engine": True
-#             }
-#     }
-# }
+
 
 from pydantic import BaseModel, ConfigDict, Field, constr, field_validator
 from typing import ClassVar, List, Union
